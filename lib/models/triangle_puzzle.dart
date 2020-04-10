90ea05998184df60888f8182c826d2b81794d364
zake-dev <zake-dev@github.com>
2020-04-10 22:04:35 +1000

    fix: triangle grid fit

--- lib/models/triangle_puzzle.dart
+++ lib/models/triangle_puzzle.dart
@@ -7,21 +7,24 @@ class TrianglePuzzle extends Center {
       : super(
           child: Column(
             mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Row(
                 mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   _buildTriangle(puzzle['puzzle'][0]),
+                  SizedBox(width: SizeConfig.safeBlockHorizontal * 2),
                   _buildTriangle(puzzle['puzzle'][1]),
                 ],
               ),
+              SizedBox(height: SizeConfig.safeBlockVertical * 2),
               Row(
                 mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   _buildTriangle(puzzle['puzzle'][2]),
+                  SizedBox(width: SizeConfig.safeBlockHorizontal * 2),
                   _buildTriangle(puzzle['puzzle'][3], isAnswer: true),
                 ],
               ),
@@ -31,49 +34,101 @@ class TrianglePuzzle extends Center {
 }
 
 Widget _buildTriangle(numbers, {isAnswer = false}) {
-  return SizedBox(
-    width: SizeConfig.safeBlockHorizontal * 40,
-    height: SizeConfig.safeBlockVertical * 20,
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(),
-            Text(
-              '${numbers[0].abs()}',
-              style: TextStyle(fontSize: SizeConfig.safeBlockHorizontal * 6),
-            ),
-            SizedBox()
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(),
-            Text(
-              '▲',
-              style: TextStyle(fontSize: SizeConfig.safeBlockHorizontal * 9),
-            ),
-            SizedBox()
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '${numbers[1].abs()}',
-              style: TextStyle(fontSize: SizeConfig.safeBlockHorizontal * 6),
-            ),
-            SizedBox(width: SizeConfig.safeBlockHorizontal * 9.5),
-            Text(
-              '${isAnswer ? '?' : numbers[2]}',
-              style: TextStyle(fontSize: SizeConfig.safeBlockHorizontal * 6),
-            )
-          ],
-        ),
-      ],
+  return Container(
+    // decoration: BoxDecoration(color: Colors.white),
+    child: SizedBox(
+      width: SizeConfig.safeBlockHorizontal * 39,
+      height: SizeConfig.safeBlockVertical * 21,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                  height: SizeConfig.safeBlockVertical * 7,
+                  width: SizeConfig.safeBlockHorizontal * 13),
+              SizedBox(
+                height: SizeConfig.safeBlockVertical * 7,
+                width: SizeConfig.safeBlockHorizontal * 13,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Text(
+                    '${isAnswer ? '?' : numbers[2]}',
+                    style:
+                        TextStyle(fontSize: SizeConfig.safeBlockHorizontal * 6),
+                  ),
+                ),
+              ),
+              SizedBox(
+                  height: SizeConfig.safeBlockVertical * 7,
+                  width: SizeConfig.safeBlockHorizontal * 13),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                  height: SizeConfig.safeBlockVertical * 7,
+                  width: SizeConfig.safeBlockHorizontal * 13),
+              SizedBox(
+                height: SizeConfig.safeBlockVertical * 7,
+                width: SizeConfig.safeBlockHorizontal * 13,
+                child: Center(
+                  child: Text(
+                    '▲',
+                    style:
+                        TextStyle(fontSize: SizeConfig.safeBlockHorizontal * 9),
+                  ),
+                ),
+              ),
+              SizedBox(
+                  height: SizeConfig.safeBlockVertical * 7,
+                  width: SizeConfig.safeBlockHorizontal * 13),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: SizeConfig.safeBlockVertical * 7,
+                width: SizeConfig.safeBlockHorizontal * 13,
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Text(
+                    '${numbers[1].abs()}',
+                    style:
+                        TextStyle(fontSize: SizeConfig.safeBlockHorizontal * 6),
+                  ),
+                ),
+              ),
+              SizedBox(
+                  height: SizeConfig.safeBlockVertical * 7,
+                  width: SizeConfig.safeBlockHorizontal * 13),
+              SizedBox(
+                height: SizeConfig.safeBlockVertical * 7,
+                width: SizeConfig.safeBlockHorizontal * 13,
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    '${numbers[0].abs()}',
+                    style:
+                        TextStyle(fontSize: SizeConfig.safeBlockHorizontal * 6),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     ),
   );
 }
